4dccb869aae1f28f5dd0d53b82a6448254dbd291
valenottaviano <valentinottaviano@gmail.com>
2020-10-11 18:09:42 -0300

    bugs fixed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const burger = document.querySelector('.burger')
 const navLinksContainer = document.querySelector('.nav-links-container')
 const cross= document.querySelector('.cross')
 const navLinks = document.querySelector('.nav-links')
+const navLink = document.querySelectorAll('.nav-links li')
 
 burger.addEventListener('click',()=>{
     navLinksContainer.classList.toggle('active');
@@ -11,4 +12,10 @@ burger.addEventListener('click',()=>{
 cross.addEventListener('click', ()=>{
     navLinksContainer.classList.remove('active');
     navLinks.classList.remove('active')
+})
+navLink.forEach(link =>{
+    link.addEventListener('click', ()=>{
+        navLinksContainer.classList.remove('active');
+     navLinks.classList.remove('active')
+    })
 })
